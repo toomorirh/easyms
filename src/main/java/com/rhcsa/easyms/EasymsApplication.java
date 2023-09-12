a016bee6c4a988c6eedb02abeac0311642190d20
toomorirh <toomori@redgat.com>
2023-09-12 12:29:12 +0900

    add openapi-spec and h2db sample

--- src/main/java/com/rhcsa/easyms/EasymsApplication.java
+++ src/main/java/com/rhcsa/easyms/EasymsApplication.java
@@ -2,8 +2,18 @@ package com.rhcsa.easyms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EntityScan("com.rhcsa.easyms.model")
+@EnableJpaRepositories("com.rhcsa.easyms.repository") 
+@OpenAPIDefinition(info = @Info(title = "Demo-Rest-App", 
+        description = "Spring Boot v3 and spring-doc open api v2 sample Apps", 
+        version = "v2.0.0")) 
 public class EasymsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rhcsa/easyms/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.rhcsa.easyms.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rhcsa.easyms.model.Tutorial;
+
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
