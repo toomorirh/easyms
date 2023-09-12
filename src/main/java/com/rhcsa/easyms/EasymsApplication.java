ed6fa8a5757fc9f0449f96bb4bbb999eb23e1631
toomorirh <toomori@redgat.com>
2023-09-12 10:52:09 +0900

    first commit

---
+++ src/main/java/com/rhcsa/easyms/EasymsApplication.java
@@ -0,0 +1,13 @@
+package com.rhcsa.easyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhcsa/easyms/EasymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhcsa.easyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
